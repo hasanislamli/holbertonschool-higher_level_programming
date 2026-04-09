25025065a341b0658752a684b1bfd76f3ba1bfe3
hasanislamli <13733@holbertonstudents.com>
2026-04-09 09:50:37 +0000

    correction #2

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-f = __import__('add_0')
+import add_0
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, f.add(a, b)))
+    print("{} + {} = {}".format(a, b, add_0.add(a, b))))
