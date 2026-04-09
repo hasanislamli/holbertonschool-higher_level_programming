e97f88a6005a991c7d3fae55e9cda4bd87d80a97
hasanislamli <13733@holbertonstudents.com>
2026-04-09 10:05:13 +0000

    Calculator1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 calculator_1 = __import__('calculator_1')
 
 if __name__ == "__main__":
     a = 10
     b = 5
-
     print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
     print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
     print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
