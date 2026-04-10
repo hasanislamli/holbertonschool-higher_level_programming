d97b11278c150c6429ac5985be15749bcd501238
hasanislamli <13733@holbertonstudents.com>
2026-04-10 17:20:32 +0000

    Matrix1

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]] or matrix == []:
+        print()
+        return
+
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
