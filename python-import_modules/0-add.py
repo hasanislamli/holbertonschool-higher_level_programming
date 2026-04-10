92aa0ec3126a394c327b98d1f2a50dd0df8303bb
hasanislamli <13733@holbertonstudents.com>
2026-04-10 16:26:22 +0000

    First Function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-f = __import__('add_0')
+from add_0 import add
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, f.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
